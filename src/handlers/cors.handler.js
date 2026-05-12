db8702aa776b117887ed4e98c1344b80e17150fd
nguyenkhacbao206 <baonk.ptit@gmail.com>
2026-05-13 04:47:34 +0700

    fix cors

--- src/handlers/cors.handler.js
+++ src/handlers/cors.handler.js
@@ -1,8 +1,12 @@
 import cors from 'cors'
 import {APP_URL_CLIENT, OTHER_URLS_CLIENT} from '@/configs'
 
+const ALLOWED_ORIGINS = [
+    'https://eloquent-sundae-135fe1.netlify.app',
+]
+
 export const corsOptions = {
-    origin: [APP_URL_CLIENT, ...OTHER_URLS_CLIENT],
+    origin: [...new Set([APP_URL_CLIENT, ...OTHER_URLS_CLIENT, ...ALLOWED_ORIGINS])],
     credentials: true,
 }
 
